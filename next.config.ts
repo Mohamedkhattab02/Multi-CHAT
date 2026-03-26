01e3ea6eafd9c42dc1d145214a27595914c6bc68
Mohamedkhattab02 <Mohammed.Khattab@e.braude.ac.il>
2026-03-26 22:34:26 +0200

    Initial commit - MultiAi project setup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  allowedDevOrigins: ["10.0.0.15"],
 };
 
 export default nextConfig;
